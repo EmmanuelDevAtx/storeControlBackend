cf12af305b99941b5f104cc81861b4f7d639b669
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-08-16 21:15:35 -0600

    update discount and put user

--- src/discounts/discounts.module.ts
+++ src/discounts/discounts.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DiscountsService } from './discounts.service';
-import { CreateNewDiscountResultResolver, DiscountsResolver } from './discounts.resolver';
+import { CreateManyDiscountsResultResolver, CreateNewDiscountResultResolver, DiscountsResolver, ShowDicountByIdResultResolver } from './discounts.resolver';
 import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Discount, DiscountSchema } from './entities/discount.entity';
 import { DiscountRepository } from './discounts.repository';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports:[
     MongooseModule.forFeature([
       {name: Discount.name, schema: DiscountSchema}
-    ])
+    ]),
+    UsersModule
   ],
   providers: [
     DiscountsResolver, 
@@ -18,7 +20,9 @@ import { DiscountRepository } from './discounts.repository';
     DiscountRepository,
 
     //Resolevrs
-    CreateNewDiscountResultResolver
+    CreateNewDiscountResultResolver,
+    CreateManyDiscountsResultResolver,
+    ShowDicountByIdResultResolver
   ]
 })
 export class DiscountsModule {}
--- src/discounts/discounts.service.ts
+++ src/discounts/discounts.service.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { CreateNewDiscountInput } from 'src/graphql/graphql';
+import { CreateManyDiscountsInput, CreateNewDiscountInput } from 'src/graphql/graphql';
 import { Discount } from './entities/discount.entity';
 import { CrudService } from 'src/core/crud.service';
 import { DiscountRepository } from './discounts.repository';
 import { ConfigService } from '@nestjs/config';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class DiscountsService extends CrudService<Discount>{
 
   constructor(
     readonly repository: DiscountRepository,
-    readonly configService: ConfigService
+    readonly configService: ConfigService,
+    readonly userService: UsersService,
   ){
     super(repository, 'DiscountService', configService)
+  }
+
+  async createManyDiscounts(input : CreateManyDiscountsInput){
+    try {
+      const dataDiscounts: any [] = [];
+      input.discounts.map((item : CreateNewDiscountInput )=>{
+        dataDiscounts.push({description: item.description, amount: item.amount, user: item.user})
+      })
+      const items = await this.repository.createMany(dataDiscounts)
+      return items
+    } catch (error) {
+      console.error(error.message)
+      throw new Error (error.message)
+    }
   }
 }
